ba1959ae111e347d8e4ebe3a39aa4eaf9707949a
Yusuf-R <y.abdulwasiu@gmail.com>
2023-03-02 15:52:29 +0100

    feat: :sparkles: implments a Bubble Sort Algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,35 +1,35 @@
 #include "sort.h"
-
 /**
- * bubble_sort - sorts an array of integers in ascending order using Bubble
- * sort algorithm.
+ * bubble_sort - sort the element using Bubble sort algorithm
  *
- * @array: an array of integers
- * @size: size of the array
+ * @array: the pointer to the array
+ * @size: the number of element in the array
  *
- * Return: void.
+ * Return: the array in the ascending order
  */
+
 void bubble_sort(int *array, size_t size)
 {
-	size_t i;
-	int temp = 0, flag = 1;
+	int num_temp, flag;
+	size_t i, j;
 
-	if (!array || !size)
+	if (!array)
 		return;
-
-	while (flag)
+	for (i = 0; i < size - 1; i++)
 	{
 		flag = 0;
-		for (i = 0; i < size - 1; i++)
+		for (j = 0; j < size - 1; j++)
 		{
-			if (array[i] > array[i + 1])
+			if (array[j] > array[j + 1])
 			{
-				temp = array[i + 1];
-				array[i + 1] = array[i];
-				array[i] = temp;
+				num_temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = num_temp;
 				flag = 1;
 				print_array(array, size);
 			}
 		}
+		if (flag == 0)
+			break;
 	}
 }
