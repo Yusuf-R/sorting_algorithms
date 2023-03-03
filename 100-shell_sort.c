55c2572961bebed4086db6cd52bc3102d0507c08
Yusuf-R <y.abdulwasiu@gmail.com>
2023-03-03 23:50:13 +0100

    feat: :sparkles: a shell sort algorithm that implements knuths intervals

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,48 +1,44 @@
 #include "sort.h"
-
 /**
- * get_max_gap - gets the a largest Knuth Sequence gap for this size
- * @size: the size of the array
+ * shell_sort - using knuth interval to sort the array
  *
- * Return: the gap size
- */
-size_t get_max_gap(size_t size)
-{
-	size_t n;
-
-	n = 1;
-	while (n < size)
-		n = n * 3 + 1;
-	return ((n - 1) / 3);
-}
-
-/**
- * shell_sort - shell_sort
- * @array: the integer array to sort
+ * @array: the pointer to the array
  * @size: the size of the array
  *
  * Return: void
  */
+
+
 void shell_sort(int *array, size_t size)
 {
-	size_t gap, i, j;
-	int temp;
 
-	if (!array || !size)
+	size_t knuth_seq, i,  gap;
+	int j, flag, store;
+
+	if (!array || !size || size < 2)
 		return;
 
+	knuth_seq = 1;
 
-	for (gap = get_max_gap(size); gap; gap = (gap - 1) / 3)
+	while (knuth_seq < size)
+		knuth_seq = (3 * knuth_seq) + 1;
+
+	for (gap = knuth_seq; gap >= 1; gap = (gap - 1) / 3)
 	{
 		for (i = gap; i < size; i++)
 		{
-			temp = array[i];
-			for (j = i; j > gap - 1 && array[j - gap] > temp; j -= gap)
+			flag = 0;
+			for (j = i - gap; j >= 0; j = j - gap)
 			{
-				array[j] = array[j - gap];
+				if (array[j + gap] > array[j])
+					break;
+				store = array[j + gap];
+				array[j + gap] = array[j];
+				array[j] = store;
+				flag = 1;
 			}
-			array[j] = temp;
 		}
-		print_array(array, size);
+		if (flag == 1)
+			print_array(array, size);
 	}
 }
