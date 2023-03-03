76d1278929405bb44e535e7a81138efd02003d5e
Yusuf-R <y.abdulwasiu@gmail.com>
2023-03-04 00:06:17 +0100

    fix: :bug: fix the print issue

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -13,7 +13,7 @@ void shell_sort(int *array, size_t size)
 {
 
 	size_t knuth_seq, i,  gap;
-	int j, flag, store;
+	int j, store;
 
 	if (!array || !size || size < 2)
 		return;
@@ -27,7 +27,6 @@ void shell_sort(int *array, size_t size)
 	{
 		for (i = gap; i < size; i++)
 		{
-			flag = 0;
 			for (j = i - gap; j >= 0; j = j - gap)
 			{
 				if (array[j + gap] > array[j])
@@ -35,10 +34,8 @@ void shell_sort(int *array, size_t size)
 				store = array[j + gap];
 				array[j + gap] = array[j];
 				array[j] = store;
-				flag = 1;
 			}
 		}
-		if (flag == 1)
-			print_array(array, size);
+		print_array(array, size);
 	}
 }
