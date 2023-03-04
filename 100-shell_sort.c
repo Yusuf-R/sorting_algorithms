af8b5b43ce16156bae6fbd067fde04f7b5f2fd41
Yusuf-R <y.abdulwasiu@gmail.com>
2023-03-04 22:36:27 +0100

    fix: :bug: nothing makes sense

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,38 +1,48 @@
 #include "sort.h"
 
 /**
- * shell_sort - sort the array using the Knuth interval
+ * get_max_gap - gets the a largest Knuth Sequence gap for this size
+ * @size: the size of the array
  *
- * @array: the pointer to the array
+ * Return: the gap size
+ */
+size_t get_max_gap(size_t size)
+{
+	size_t n;
+
+	n = 1;
+	while (n < size)
+		n = n * 3 + 1;
+	return ((n - 1) / 3);
+}
+
+/**
+ * shell_sort - shell_sort
+ * @array: the integer array to sort
  * @size: the size of the array
  *
  * Return: void
  */
-
 void shell_sort(int *array, size_t size)
 {
-	size_t knuth_seq, i, j;
+	size_t gap, i, j;
 	int temp;
 
-	knuth_seq = 1;
-	while (knuth_seq < size)
-		knuth_seq = (3 * knuth_seq) + 1;
+	if (!array || !size)
+		return;
 
-	while (knuth_seq > 0)
+
+	for (gap = get_max_gap(size); gap; gap = (gap - 1) / 3)
 	{
-		for (i = knuth_seq; i < size; i++)
+		for (i = gap; i < size; i++)
 		{
 			temp = array[i];
-			j = i;
-
-			while (j >= knuth_seq && array[j - knuth_seq] >= temp)
+			for (j = i; j > gap - 1 && array[j - gap] > temp; j -= gap)
 			{
-				array[j] = array[j - knuth_seq];
-				j = j - knuth_seq;
+				array[j] = array[j - gap];
 			}
 			array[j] = temp;
 		}
-		knuth_seq = (knuth_seq - 1) / 3;
 		print_array(array, size);
 	}
 }
