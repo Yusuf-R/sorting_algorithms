f603d7c00ef095b91d8426495e0c972632317c97
Yusuf-R <y.abdulwasiu@gmail.com>
2023-03-04 16:11:51 +0100

    frustrated

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,6 +1,7 @@
 #include "sort.h"
+
 /**
- * shell_sort - using knuth interval to sort the array
+ * shell_sort - sort the array using the Knuth interval
  *
  * @array: the pointer to the array
  * @size: the size of the array
@@ -8,37 +9,30 @@
  * Return: void
  */
 
-
 void shell_sort(int *array, size_t size)
 {
-
-	size_t knuth_seq, i,  gap;
-	int j, flag, store;
-
-	if (!array || !size || size < 2)
-		return;
+	size_t knuth_seq, i, j;
+	int temp;
 
 	knuth_seq = 1;
-
 	while (knuth_seq < size)
 		knuth_seq = (3 * knuth_seq) + 1;
 
-	for (gap = knuth_seq; gap; gap = (gap - 1) / 3)
+	while (knuth_seq > 0)
 	{
-		for (i = gap; i < size; i++)
+		for (i = knuth_seq; i < size; i++)
 		{
-			flag = 0;
-			for (j = i - gap; j >= 0; j = j - gap)
+			temp = array[i];
+			j = i;
+
+			while (j >= knuth_seq && array[j - knuth_seq] >= temp)
 			{
-				if (array[j + gap] > array[j])
-					break;
-				store = array[j + gap];
-				array[j + gap] = array[j];
-				array[j] = store;
-				flag = 1;
+				array[j] = array[j - knuth_seq];
+				j = j - knuth_seq;
 			}
+			array[j] = temp;
 		}
-		if (flag == 1)
-			print_array(array, size);
+		knuth_seq = (knuth_seq - 1) / 3;
+		print_array(array, size);
 	}
 }
